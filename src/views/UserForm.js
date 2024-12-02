261882bd515eaa6baafe82f6c7b3a8e89eebba62
Eduardo Fernando Farfus <eduardo.farfus@angellira.com.br>
2024-12-02 17:46:43 -0300

    finalziado app com crud completo

--- src/views/UserForm.js
+++ src/views/UserForm.js
@@ -1,49 +1,56 @@
-import React, {useState} from 'react';
-import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
+import React, { useState, useContext } from 'react'
+import { Text, View, TextInput, StyleSheet, Button } from 'react-native'
+import UsersContext from '../context/UserContext'
 
 export default ({route, navigation}) => {
-  const [user, setUser] = useState(route?.params || {});
+    const [user, setUser] = useState(route.params ? route.params : {})
+    const { dispatch } = useContext(UsersContext)
 
-  return (
-    <View style={style.form}>
-      <Text>Name</Text>
-      <TextInput
-        style={style.input}
-        onChange={name => setUser({...user, name})}
-        placeholder="Informe o nome "
-        value={user.name}></TextInput>
-
-      <Text>Email</Text>
-      <TextInput
-        style={style.input}
-        onChange={email => setUser({...user, email})}
-        placeholder="Informe o email "
-        value={user.email}></TextInput>
-
-      <Text>URL do avatar</Text>
-      <TextInput
-        style={style.input}
-        onChange={avatarUrl => setUser({...user, avatarUrl})}
-        placeholder="Informe a URL do avatar "
-        value={user.avatarUrl}></TextInput>
-
-        <Button title='Salvar' onPress={() => {
-            navigation.goBack()
-        }}>
-
-        </Button>
-    </View>
-  );
-};
+    return (
+        <View style={style.form}>
+            <Text>Nome</Text>
+            <TextInput
+                style={style.input}
+                onChangeText={name => setUser({...user, name})}
+                placeholder="Informe o Nome"
+                value={user.name}
+            />
+            <Text>Email</Text>
+            <TextInput
+                style={style.input}
+                onChangeText={email => setUser({...user, email})}
+                placeholder="Informe o E-mail"
+                value={user.email}
+            />
+            <Text>URL do Avatar</Text>
+            <TextInput
+                style={style.input}
+                onChangeText={avatarUrl => setUser({...user, avatarUrl})}
+                placeholder="Informe a URL do Avatar"
+                value={user.avatarUrl}
+            />
+            <Button
+                title="Salvar"
+                onPress={() => {
+                    dispatch({
+                        type: user.id ? 'updateUser' : 'createUser',
+                        payload: user,
+                    })
+                    navigation.goBack()
+                }}
+            />
+        </View>
+    )
+}
 
 const style = StyleSheet.create({
-  input: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 15,
-  },
-  form: {
-    padding: 12,
-  },
-});
+    form: {
+        padding: 12
+    },
+    input: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 15,
+    }
+})
--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,44 +1,20 @@
-import React from 'react';
-import users from '../data/users';
-import {FlatList, View, StyleSheet, Alert} from 'react-native';
-import {ListItem, Avatar, Icon} from 'react-native-elements';
+import React, { useContext } from 'react';
+import { View, FlatList, Alert, StyleSheet } from 'react-native';
+import { ListItem, Avatar, Icon, Button } from 'react-native-elements';
+import UsersContext from '../context/UserContext';
 
 export default props => {
-  function getUserItem({item: user}) {
-    return (
-      <ListItem
-        key={user.id}
-        bottomDivider
-        onPress={() => props.navigation.navigate('UserForm', user)}>
-        <Avatar source={{uri: user.avatarUrl}} />
-        <ListItem.Content>
-          <ListItem.Title>{user.name}</ListItem.Title>
-          <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
-        </ListItem.Content>
-        <Icon
-          name="edit"
-          type="material"
-          color="orange"
-          size={25}
-          onPress={() => props.navigation.navigate('UserForm', user)}
-        />
-        <Icon
-          name="delete"
-          type="material"
-          color="red"
-          size={25}
-          onPress={() => confirmUserDeletion(user)}
-        />
-      </ListItem>
-    );
-  }
+  const { state, dispatch } = useContext(UsersContext);
 
   function confirmUserDeletion(user) {
     Alert.alert('Excluir Usuário', 'Deseja excluir o usuário?', [
       {
         text: 'Sim',
         onPress: () => {
-          console.warn("Delete " + user.id)
+          dispatch({
+            type: 'deleteUser',
+            payload: user,
+          });
         },
       },
       {
@@ -47,15 +23,52 @@ export default props => {
     ]);
   }
 
+  function getActions(user) {
+    return (
+      <>
+        <Button
+          onPress={() => props.navigation.navigate('UserForm', user)}
+          type="clear"
+          icon={<Icon name="edit" size={25} color="orange" />}
+        />
+        <Button
+          onPress={() => confirmUserDeletion(user)}
+          type="clear"
+          icon={<Icon name="delete" size={25} color="red" />}
+        />
+      </>
+    );
+  }
+
+  function getUserItem({ item: user }) {
+    return (
+      <ListItem
+        bottomDivider
+        onPress={() => props.navigation.navigate('UserForm', user)}>
+        <Avatar source={{ uri: user.avatarUrl }} />
+        <ListItem.Content>
+          <ListItem.Title>{user.name}</ListItem.Title>
+          <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+        </ListItem.Content>
+        {getActions(user)}
+      </ListItem>
+    );
+  }
+
   return (
-    <View >
+    <View style={styles.container}>
       <FlatList
         keyExtractor={user => user.id.toString()}
-        data={users}
+        data={state.users}
         renderItem={getUserItem}
       />
     </View>
   );
 };
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
